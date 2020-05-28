7f1752dedb6a9e75af0228bbcdf181c2c23b1456
Eric Andrade <vloxflox@gmail.com>
2020-05-28 17:24:46 -0300

    puxando tudo

---
+++ js/rank.js
@@ -0,0 +1,45 @@
+var choice1 = false;
+var choice2 = false;
+
+colorUpdate1();
+colorUpdate2();
+
+function updateChild(){
+ if ((choice1 || choice2) && !(choice1 && choice2) ){
+  if(choice1){
+   document.getElementById("child").innerHTML = document.getElementById("parent1").innerHTML;
+  }else{
+   document.getElementById("child").innerHTML = document.getElementById("parent2").innerHTML;
+  }
+ }
+}
+
+function click1(){
+ choice1 = !choice1;
+ updateChild();
+ colorUpdate1();
+}
+
+function click2() {
+ choice2 = !choice2;
+ updateChild();
+ colorUpdate2();
+}
+
+function colorUpdate1(){
+ var x = document.getElementById("parent1");
+ if (choice1){
+  x.style.background = 'paleGreen';
+ }else{
+  x.style.background = 'salmon';
+ }
+}
+
+function colorUpdate2() {
+ var x = document.getElementById("parent2");
+ if (choice2) {
+  x.style.background = 'paleGreen';
+ } else {
+  x.style.background = 'salmon';
+ }
+}
